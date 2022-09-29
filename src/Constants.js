8d41ac068aca94b83fbabfafeca5f9fc7b997a43
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-09-29 18:09:41 +0200

    Non printable characters list

---
+++ src/Constants.js
@@ -0,0 +1,15 @@
+const emptyParagraphCharacter = "❡";
+const endParagraphCharacter = "¶";
+const newLineCharacter = "⏎";
+const pageBreakCharacter = "↡";
+const simplySpaceCharacter = "▪";
+const tabulationCharacter = "￫";
+
+const nonPrintableCharacters = [
+  emptyParagraphCharacter,
+  endParagraphCharacter,
+  newLineCharacter,
+  pageBreakCharacter,
+  simplySpaceCharacter,
+  tabulationCharacter,
+];
